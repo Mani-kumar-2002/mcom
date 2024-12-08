9f009b6a85722901ea2f4ea6276b940db2e16b85
Mani-kumar-2002 <manikumar6423@gmail.com>
2024-12-08 17:38:45 +0530

    Fixed CORS error

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mcom-backend.onrender.com",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
